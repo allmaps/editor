84dda5b359613579785ad0160d2a47d5aa0b1bcc
Bert Spaan <bert.spaan@gmail.com>
2021-07-26 16:04:09 +0200

    new @allmaps module versions

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2020: true,
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
